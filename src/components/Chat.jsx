8922a97e17f3e232edc081fbd0bbd8573e0a6db6
Alexander Malinov <alieksandr.malinov@mail.ru>
2020-11-01 22:00:22 +0300

    add react component - login form

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,11 @@
-import { extend } from 'lodash';
 import React from 'react';
+import LoginForm from './forms/LoginForm';
+import UserRegistrationForm from './forms/UserRegistrationForm';
 
 class Chat extends React.Component {
-  
+  render() {
+    
+  }
 }
 
 export default Chat;
