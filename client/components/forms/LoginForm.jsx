f17e48bf36861b77d574e05654d660f8c9ea725f
Alexander Malinov <Alexander.Malinov@comindware.com>
2021-02-07 21:16:46 +0300

    Add formic library to start forms

--- client/components/forms/LoginForm.jsx
+++ client/components/forms/LoginForm.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
+import { Formik, Field, Form } from 'formik';
 
-export class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  // onTextType(e) {
-    
-  // }
-
-  render() {
-    return (
-      <form className="user-form">
+export const LoginForm = () => {
+  return (
+    <Formik
+      initialValues={{
+        login: '',
+        password: ''
+      }}
+      onSubmit={(values) => console.log(values)}
+    >
+      <Form className="user-form">
         <h3 className="user-form__form-header">Вход</h3>
         <div className="form-group">
-          <label htmlFor="Login">Логин</label>
-          <input onChange={this.onTextType} type="text" className="form-control" id="Login" placeholder="Введите логин" />
+          <label htmlFor="login">Логин</label>
+          <Field required type="text" className="form-control" id="login" name="login" placeholder="Введите логин" />
         </div>
         <div className="form-group">
           <label htmlFor="password">Пароль</label>
-          <input type="password" className="form-control" id="password" placeholder="Введите пароль" />
+          <Field required type="password" className="form-control" id="password" name="password" placeholder="Введите пароль" />
         </div>
         <button type="submit" className="btn btn-primary user-form__submit-button">Войти</button>
-      </form>
-    );
-  }
+      </Form>
+    </Formik>
+  );
 };
--- client/components/forms/UserRegistrationForm.jsx
+++ client/components/forms/UserRegistrationForm.jsx
@@ -1,33 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { registration } from '../../actions/index.js';
+import { Formik, Field, Form } from 'formik';
 
 const actionCreators = { registration };
 
 const UserRegistrationForm = (props) => {
-  const onSubmit = (event) => {
-    const { registration } = props;
-    event.preventDefault();
-    registration({ formData: 'boom!' });
-  };
+  // const onSubmit = (event) => {
+  //   const { registration } = props;
+  //   event.preventDefault();
+  //   registration({ formData: 'boom!' });
+  // };
 
   return (
-    <form onSubmit={onSubmit} className="user-form">
-      <h3 className="user-form__form-header">Регистрация</h3>
-      <div className="form-group">
-        <label htmlFor="Login">Логин</label>
-        <input type="text" className="form-control" id="Login" placeholder="Введите логин" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Пароль</label>
-        <input type="password" className="form-control" id="password" placeholder="Введите пароль" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password-conf">Подтверждение пароля</label>
-        <input type="password" className="form-control" id="password-conf" placeholder="Введите пароль снова" />
-      </div>
-      <button type="submit" className="btn btn-primary user-form__submit-button">Зарегистрироваться</button>
-    </form>
+    <Formik
+      initialValues={{
+        login: '',
+        password: '',
+        passwordConfirmation: ''
+      }}
+      onSubmit={(values) => console.log(values)}
+    >
+      <Form className="user-form">
+        <h3 className="user-form__form-header">Регистрация</h3>
+        <div className="form-group">
+          <label htmlFor="login">Логин</label>
+          <Field required type="text" className="form-control" name="login" id="login" placeholder="Введите логин" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Пароль</label>
+          <Field required type="password" className="form-control" name="password" id="password" placeholder="Введите пароль" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password-conf">Подтверждение пароля</label>
+          <Field required type="password" className="form-control" name="passwordConfirmation" id="password-conf" placeholder="Введите пароль снова" />
+        </div>
+        <button type="submit" className="btn btn-primary user-form__submit-button">Зарегистрироваться</button>
+      </Form>
+    </Formik>
   );
 };
 
