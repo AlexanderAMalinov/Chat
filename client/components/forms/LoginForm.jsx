ab00856d1be70229bd74ae291be598177d07150d
Alexander Malinov <Alexander.Malinov@comindware.com>
2021-02-08 23:48:05 +0300

    Move password validation from login form to registration form

--- client/components/forms/LoginForm.jsx
+++ client/components/forms/LoginForm.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
-import ValidationService from '../../services/ValidationService';
 
 export const LoginForm = () => {
   return (
@@ -11,21 +10,18 @@ export const LoginForm = () => {
       }}
       onSubmit={(values) => console.log(values)}
     >
-      {({ errors, touched }) => (
-        <Form className="user-form">
-          <h3 className="user-form__form-header">Вход</h3>
-          <div className="form-group">
-            <label htmlFor="login">Логин</label>
-            <Field type="text" className="form-control" id="login" name="login" placeholder="Введите логин" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Пароль</label>
-            <Field validate={ValidationService.validatePassword} type="password" className="form-control" id="password" name="password" placeholder="Введите пароль" />
-            {errors.password && touched.password && <div className="user-form__incorrect-notification">{errors.password}</div>}
-          </div>
-          <button type="submit" disabled={(!touched.password && !touched.login) || errors.password} className="btn btn-primary user-form__submit-button">Войти</button>
-        </Form>
-      )}
+      <Form className="user-form">
+        <h3 className="user-form__form-header">Вход</h3>
+        <div className="form-group">
+          <label htmlFor="login">Логин</label>
+          <Field type="text" className="form-control" id="login" name="login" placeholder="Введите логин" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Пароль</label>
+          <Field type="password" className="form-control" id="password" name="password" placeholder="Введите пароль" />
+        </div>
+        <button type="submit" className="btn btn-primary user-form__submit-button">Войти</button>
+      </Form>
     </Formik>
   );
 };
--- client/components/forms/UserRegistrationForm.jsx
+++ client/components/forms/UserRegistrationForm.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { registration } from '../../actions/index.js';
 import { Formik, Field, Form } from 'formik';
+import ValidationService from '../../services/ValidationService';
 
 const actionCreators = { registration };
 
 const UserRegistrationForm = (props) => {
-  // const onSubmit = (event) => {
-  //   const { registration } = props;
-  //   event.preventDefault();
-  //   registration({ formData: 'boom!' });
-  // };
 
   return (
     <Formik
@@ -21,22 +17,25 @@ const UserRegistrationForm = (props) => {
       }}
       onSubmit={(values) => console.log(values)}
     >
-      <Form className="user-form">
-        <h3 className="user-form__form-header">Регистрация</h3>
-        <div className="form-group">
-          <label htmlFor="login">Логин</label>
-          <Field required type="text" className="form-control" name="login" id="login" placeholder="Введите логин" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Пароль</label>
-          <Field required type="password" className="form-control" name="password" id="password" placeholder="Введите пароль" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password-conf">Подтверждение пароля</label>
-          <Field required type="password" className="form-control" name="passwordConfirmation" id="password-conf" placeholder="Введите пароль снова" />
-        </div>
-        <button type="submit" className="btn btn-primary user-form__submit-button">Зарегистрироваться</button>
-      </Form>
+      {({ errors, touched }) => (
+        <Form className="user-form">
+          <h3 className="user-form__form-header">Регистрация</h3>
+          <div className="form-group">
+            <label htmlFor="login">Логин</label>
+            <Field type="text" className="form-control" name="login" id="login" placeholder="Введите логин" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Пароль</label>
+            <Field validate={ValidationService.validatePassword} type="password" className="form-control" name="password" id="password" placeholder="Введите пароль" />
+            {errors.password && touched.password && <div className="user-form__incorrect-notification">{errors.password}</div>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="password-conf">Подтверждение пароля</label>
+            <Field type="password" className="form-control" name="passwordConfirmation" id="password-conf" placeholder="Введите пароль снова" />
+          </div>
+          <button type="submit" className="btn btn-primary user-form__submit-button">Зарегистрироваться</button>
+        </Form>
+      )}
     </Formik>
   );
 };
